c196b1a0a81904a76720270c6ef25e6bcf6ce6b8
keisukeitopro <keisuke.ito.programmer@gmail.com>
2019-08-23 21:16:33 +0900

    replace the message

--- sample.rb
+++ sample.rb
@@ -1,10 +1,19 @@
 def hello
-  p "hello, Git."
+  p "hello, GitHub."
 end
+
 hello
 
+# 👇②
+# def hello
+#   p "hello, Git."
+# end
+# hello
+
+# 👇①
 # def hello
 #   p "hello, world."
 # end
 
 # hello
+
